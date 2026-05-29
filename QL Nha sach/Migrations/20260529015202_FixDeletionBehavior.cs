0ec3856b9169949e5201aafe80a1a9f04bbf2ac8
hoang-cong <conghoang20063110@gmail.com>
2026-05-29 10:19:08 +0700

    Recovered UI changes

---
+++ QL Nha sach/Migrations/20260529015202_FixDeletionBehavior.cs
@@ -0,0 +1,210 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QL_Nha_sach.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixDeletionBehavior : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookAuthors_Books_BookId",
+                table: "BookAuthors");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookGenres_Books_BookId",
+                table: "BookGenres");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Publishers_PublisherId",
+                table: "Books");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ImportDetails_Books_BookId",
+                table: "ImportDetails");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Imports_Users_UserId",
+                table: "Imports");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_InvoiceDetails_Books_BookId",
+                table: "InvoiceDetails");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Invoices_Users_UserId",
+                table: "Invoices");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PromotionTargets_Books_BookId",
+                table: "PromotionTargets");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookAuthors_Books_BookId",
+                table: "BookAuthors",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "BookId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookGenres_Books_BookId",
+                table: "BookGenres",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "BookId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Publishers_PublisherId",
+                table: "Books",
+                column: "PublisherId",
+                principalTable: "Publishers",
+                principalColumn: "PublisherId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ImportDetails_Books_BookId",
+                table: "ImportDetails",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "BookId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Imports_Users_UserId",
+                table: "Imports",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_InvoiceDetails_Books_BookId",
+                table: "InvoiceDetails",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "BookId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Invoices_Users_UserId",
+                table: "Invoices",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PromotionTargets_Books_BookId",
+                table: "PromotionTargets",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "BookId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookAuthors_Books_BookId",
+                table: "BookAuthors");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookGenres_Books_BookId",
+                table: "BookGenres");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Publishers_PublisherId",
+                table: "Books");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ImportDetails_Books_BookId",
+                table: "ImportDetails");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Imports_Users_UserId",
+                table: "Imports");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_InvoiceDetails_Books_BookId",
+                table: "InvoiceDetails");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Invoices_Users_UserId",
+                table: "Invoices");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PromotionTargets_Books_BookId",
+                table: "PromotionTargets");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookAuthors_Books_BookId",
+                table: "BookAuthors",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "BookId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookGenres_Books_BookId",
+                table: "BookGenres",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "BookId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Publishers_PublisherId",
+                table: "Books",
+                column: "PublisherId",
+                principalTable: "Publishers",
+                principalColumn: "PublisherId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ImportDetails_Books_BookId",
+                table: "ImportDetails",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "BookId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Imports_Users_UserId",
+                table: "Imports",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_InvoiceDetails_Books_BookId",
+                table: "InvoiceDetails",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "BookId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Invoices_Users_UserId",
+                table: "Invoices",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PromotionTargets_Books_BookId",
+                table: "PromotionTargets",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "BookId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
